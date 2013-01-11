6bad2a1e213f64c57d8876a6b1b2569154f4cf1b
Johan <joacub@hotmail.com>
2013-01-11 19:19:27 +0100

    primer commint

---
+++ config/module.config.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MnsFormAnnotation;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+return array(
+    'mns_cache_config' => array(
+        'adapter' => array(
+            'name' => 'filesystem',
+            'options' => array(
+                'ttl' => 100,
+                'dir_permission' => 0755,
+                'cache_dir' => 'data/cache',
+            ),
+        ),
+        'plugins' => array(
+            array(
+                'name' => 'serializer',
+                'exception_handler' => array('throw_exceptions' => false),
+            )
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'MnsFormAnnotation\Controller\MnsFormAnnotation' => 'MnsFormAnnotation\Controller\IndexController',
+        ),
+    ),
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'form' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/form[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'MnsFormAnnotation\Controller\MnsFormAnnotation',
+                        'action' => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'MnsFormAnnotation' => __DIR__ . '/../view',
+        ),
+    ),   
+);
+?>
---
+++ src/MnsFormAnnotation/Service/FormGenerator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MnsFormAnnotation\Service;
+
+use Zend\Cache\StorageFactory;
+use Zend\Form\Annotation\AnnotationBuilder;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Use ZF2 form annotations builder to generate a form
+ *
+ * @author nshankar
+ */
+class FormGenerator implements FactoryInterface {
+
+    protected $className;
+    protected $keyName;
+
+    public function setClass($className) {
+        $this->className = $className;
+        //remove special characters from class name in cache
+        $this->keyName = str_replace('\\', '_', $className);
+        return $this;   //fluent interface
+    }
+
+    public function getForm() {
+        $builder = new AnnotationBuilder();
+        if ($this->cache) {
+            if (!($this->cache->hasItem($this->keyName))) {
+                $form = $builder->createForm(new $this->className);
+                $this->cache->setItem($this->keyName, $form);
+            } else {
+                //var_dump("Cache hit");
+                $form = $this->cache->getItem($this->keyName);
+            }
+        } else {    //Do not use cache
+            $form = $builder->createForm(new $this->className);
+        }
+        return $form;
+    }
+
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        $config = $serviceLocator->get('config');
+        if (isset($config['mns_cache_config'])) {
+            $cacheConfig = $config['mns_cache_config'];
+            //initialize cache
+            $this->cache = StorageFactory::factory($cacheConfig);
+        }
+        return $this;  //fluent interface
+    }
+
+}
+
+?>
