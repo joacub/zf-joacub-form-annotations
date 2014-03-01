3c0fe6679f080dbb98d32c5a6519bef845f78a64
Johan <joacub@gmail.com>
2014-03-01 14:20:48 +0100

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- config/module.config.php
+++ config/module.config.php
@@ -49,8 +49,9 @@ return array(
     ),
     'view_manager' => array(
         'template_path_stack' => array(
-            'MnsFormAnnotation' => __DIR__ . '/../view',
+            __DIR__ . '/../view',
         ),
+        'template_map' => include __DIR__  .'/../template_map.php',
     ),   
     'doctrine' => array(
         'connection' => array(
